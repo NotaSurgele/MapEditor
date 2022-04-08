bc0ee6440093c006341e4ee6fa7fc09f3e0ecb1d
NotaSurgele <alexis.picard@epitech.eu>
2022-04-08 22:39:13 +0200

    write data into array

--- core/src/com/editor/game/MapEditor.java
+++ core/src/com/editor/game/MapEditor.java
@@ -3,17 +3,10 @@ package com.editor.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
-
-import java.awt.*;
 
 public class MapEditor extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -56,7 +49,6 @@ public class MapEditor extends ApplicationAdapter {
 		}
 
 		isLock = Gdx.input.isKeyJustPressed(Input.Keys.SPACE) != isLock;
-
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		camera.update();
